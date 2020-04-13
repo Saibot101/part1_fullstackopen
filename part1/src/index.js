ea9c5ca2804f0972f27be7003c3c426415cda6f6
Saibot101 <tobiasmaas98@web.de>
2020-04-13 15:21:37 +0200

    Part2C Teil 2 finished

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,80 +1,57 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-    // save clicks of each button to own state
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [bad, setBad] = useState(0);
+const App = (props) => {
+    const [selected, setSelected] = useState(0);
+    const [votes, setVotes] = useState([0,0,0,0,0,0]);
+    const [mostVotes, setMostVotes] = useState([0,0]);
 
-    const handleGood = () => {
-        setGood(good+1)
+    const nextRandom = () => {
+        let random = Math.floor(Math.random() * Math.floor(6)) ;
+        //console.log(random);
+        setSelected(random);
     };
-    const handleNeutral = () => {
-        setNeutral(neutral+1)
-    };
-    const handleBad = () => {
-        setBad(bad+1)
+
+    const updateVote = () => {
+        let ary = votes.slice();
+        ary[selected] += 1;
+        setVotes(ary);
+
+        let ary2 = mostVotes.slice();
+
+        if(ary2[0] < ary[selected]){
+            ary2[0] = ary[selected];
+            ary2[1] = selected;
+        }
+
+        setMostVotes(ary2);
     };
 
     return (
         <div>
-            <h1>give feedback</h1>
-            <Button handleClick={handleGood} text='good'/>
-            <Button handleClick={handleNeutral} text='neutral'/>
-            <Button handleClick={handleBad} text='bad'/>
-
-            <Statistics good={good} neutral={neutral} bad={bad} />
+            <h1>Anecdote of the day</h1>
+            <p>{props.anecdotes[selected]}</p>
+            <p>has {votes.join(' ')} votes</p>
+            <p>has {votes[selected]} votes</p>
+            <button onClick={() => updateVote()}>vote</button>
+            <button onClick={() => nextRandom()} >next anecdote</button>
+            <h1>Anecdote with the most votes</h1>
+            <p>{props.anecdotes[mostVotes[1]]}</p>
+            <p>has {mostVotes[0]} votes</p>
         </div>
     )
 };
 
-const Button = ({handleClick, text}) => (<button onClick={handleClick}> {text} </button>);
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
 
-const Statistics = ({good, neutral, bad}) => {
-    let all = good+neutral+bad;
-    let average = (good-bad)/all;
-    let positive = good/all;
-
-    if (good === 0 && neutral === 0 && bad===0){
-        return (
-            <div>
-                <p>No feedback given</p>
-            </div>
-        )
-    }
-
-    return(
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>
-                            <h1>statistics</h1>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <SingleStatistic value={good}/>
-                    <SingleStatistic value={neutral}/>
-                    <SingleStatistic value={bad}/>
-                    <SingleStatistic value={all}/>
-                    <SingleStatistic value={average.toString()}/>
-                    <SingleStatistic value={positive.toString() + ' %'}/>
-                </tbody>
-            </table>
-        </div>
-    )
-};
-
-const SingleStatistic = ({value}) =>{
-    return (
-        <tr>
-            <td>{value}</td>
-        </tr>
-    )
-};
-
-ReactDOM.render(<App />,
+ReactDOM.render(
+    <App anecdotes={anecdotes} />,
     document.getElementById('root')
 );
